f141fb0164ffe9bc49740f9cf88d3ec15421a334
Anjana Devi <anjanadevi411@gmail.com>
2021-07-30 12:02:44 +0200

    first commit to task management assignment

---
+++ main_send.js
@@ -0,0 +1,66 @@
+//variables
+
+const form_task = document.querySelector('#task-form');
+const task_list = document.querySelector('.list-ul');
+const remove_all = document.querySelector('.remove-all');
+const input_task = document.querySelector('#task');
+const task_finish = document.querySelector('.list-ul');
+
+//event listeners
+
+loadEventListeners();
+
+
+function loadEventListeners(){
+    //adding Todo list one by one
+    form_task.addEventListener('submit',addTask);
+
+    //removing todo task one at a time
+    task_list.addEventListener('click',removeTask);
+
+    //removing todo all tasks all at once
+    remove_all.addEventListener('click',removeAll);
+
+    //adding background color, strike through, adding tick to the task
+    task_finish.addEventListener('click',finishedtask);
+
+}
+
+function addTask(e){
+    if(input_task.value === ''){
+        alert('Enter task');
+    }
+    //create li element
+    const task_li = document.createElement('li');
+    //create classname for li element
+    task_li.className = 'list-li';
+    
+    //appending the input value to li element
+    task_li.appendChild(document.createTextNode(input_task.value));
+    const link = document.createElement('a');
+    link.className = 'remove-a';
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    task_li.appendChild(link)
+    task_list.appendChild(task_li);
+
+    input_task.value = '';
+    e.preventDefault();
+}
+
+
+function removeTask(e){
+    if(e.target.parentElement.classList.contains('remove-a')){
+        e.target.parentElement.parentElement.remove();
+    }
+    
+    e.preventDefault();
+}
+
+function removeAll(e){
+    task_list.innerHTML = '';
+
+    e.preventDefault();
+}
+
+
+
